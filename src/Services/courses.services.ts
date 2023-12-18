24d8f6f723888d62262b00cf2b52bf4f7e352780
Shariful Islam <sharifsharifulislam061@gmail.com>
2023-12-18 21:52:55 +0600

    courses services added

---
+++ src/Services/courses.services.ts
@@ -0,0 +1,38 @@
+import { Course } from '@prisma/client'
+import prisma from '../shared/prisma'
+
+const createCourse = async (courseData: Course) => {
+  const course = await prisma.course.create({ data: courseData })
+  return course
+}
+const getCourse = async (courseCode: string) => {
+  const course = await prisma.course.findUnique({ where: { courseCode } })
+  if (!course) {
+    throw new Error('Course with this id does not exist')
+  }
+  return course
+}
+const getAllCourse = async () => await prisma.course.findMany({})
+const updateCourse = async (
+  courseCode: string,
+  courseData: Partial<Course>,
+) => {
+  await getCourse(courseCode)
+  const result = await prisma.course.update({
+    where: { courseCode },
+    data: courseData,
+  })
+  return result
+}
+const deleteCourse = async (courseCode: string) => {
+  await getCourse(courseCode)
+  const result = await prisma.course.delete({ where: { courseCode } })
+  return result
+}
+export const courseServices = {
+  createCourse,
+  updateCourse,
+  getCourse,
+  deleteCourse,
+  getAllCourse,
+}
